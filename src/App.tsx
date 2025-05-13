0998322925e5fbb84442f4db89772a37c036d1fa
DanielErazo <daniel96.florez@gmail.com>
2025-05-12 22:48:43 -0500

    package tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { SideBar } from './components/SideBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <SideBar/>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
---
+++ src/components/SideBar.tsx
@@ -0,0 +1,10 @@
+
+export const SideBar = () => {
+  return (
+    <nav className="bg-gray-800">
+        <div className="">
+            SideBar
+        </div>
+    </nav>
+  )
+}
